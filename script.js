77806db4614c2696f3ccc9690a7cf1d54b11bcf2
athulcherukunnath <athulcherukunnath@gmail.com>
2022-05-10 22:28:13 +0530

    changes2

--- script.js
+++ script.js
@@ -44,4 +44,47 @@ function displaylist(){
 function display(data){
     var list = JSON.parse(data);
     let table = document.getElementById("todotable");
+
+    for(var i=0;i<list.length;i++){
+        let rowno = table.rows.length;
+        var row = table.insertRow(rowno);
+       
+        var cell1 = row.insertCell(0);
+        cell1.innerHTML = list[i].id;
+       
+        var cell2 = row.insertCell(1);
+        cell2.innerHTML = list[i].title;
+
+        var cell3 = row.insertCell(2);
+        var element = document.createElement("input");
+        element.type = "checkbox";
+
+        if(list[i].completed == true){
+            element.setAttribute("checked","true");
+            element.setAttribute("disabled","true");
+        }
+        element.addEventListener('change',(Event)=>{
+            if(Event.currentTarget.checked){
+                count++;
+                countcheck();
+            }
+            else{
+                count--;
+            }
+        })
+        cell3.appendChild(element);
+    }
+}
+// checking the count
+var count = 0;
+function countcheck(){
+
+    let promise = new Promise(function(resolve,reject){
+        if(count==5){
+            resolve("Congrats, you have been successfully completed the 5 Tasks");
+        }
+    })
+    promise.then(function(t){
+        alert(t);
+    })
 }
